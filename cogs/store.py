cf72f53891e2d68dac3b4d455689850c18d5bbc8
luxzk01-beep <luxzk01@gmail.com>
2026-08-04 15:00:05 -0300

    Update store.py

--- cogs/store.py
+++ cogs/store.py
@@ -143,4 +143,4 @@ class CatalogView(discord.ui.View):
         )
         pix_embed.add_field(name="Produto", value=product['name'], inline=True)
         pix_embed.add_field(name="Valor", value=f"R$ {product['price']:.2f}", inline=True)
-        pix_embed.add_field(name="Chave PIX (Copia e Cola)", value=f"```{pix_code}
+        pix_embed.add_field(name="Chave PIX (Copia e Cola)", value=f"```{pix_code}```")
